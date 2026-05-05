6662d31751a32e2e54f5003f23f07d2f23f19c80
balcoder <des.barrett@gmail.com>
2026-05-05 08:47:29 +0100

    created signin api route & cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { userInfo } from "node:os";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,31 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, " invalid authentication credentials"));
+    // we have valid user and password so authenticate user throughout site by using cookie
+    // with JWT
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // remove the password
+    const { password: pass, ...rest } = validUser._doc;
+    // save token as cookie session
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
